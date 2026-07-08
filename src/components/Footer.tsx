2016899abfa17a07d6bbb3dbf1192af7655d455f
Sercank91 <sercan.karagoz@hotmail.com>
2026-07-08 16:37:32 +0200

    Update Footer.tsx
    text geändert

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
         className="w-full py-4 px-8 text-right border-t border-[#c6c6c6]"
       >
         <p className="text-gray-600 text-xs">
-          1.0.5v © 2025 MAS Project GmbH für Speditionen
+          1.0.5v © 2026 Atilla Spedition GmbH Coded with Love (Sercan K.)
         </p>
       </footer>
     );
